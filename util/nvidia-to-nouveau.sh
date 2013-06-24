2a9b85aadf4c857d66b98513ab18e63224d9fd50
Samuel Pitoiset <samuel.pitoiset@gmail.com>
2013-06-24 17:07:52 +0200

    util: Add nvidia-to-nouveau.sh and nouveau-to-nvidia.sh

---
+++ util/nvidia-to-nouveau.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# nvidia -> nouveau
+
+set -e
+
+# check if root
+if [[ $EUID -ne 0 ]]; then
+    echo "You must be root to run this script. Aborting...";
+    exit 1;
+fi
+
+sed -i 's/#*MODULES="nouveau"/MODULES="nouveau"/' /etc/mkinitcpio.conf
+
+pacman -Rdds --noconfirm nvidia nvidia-utils nvidia-libgl #lib32-nvidia-libgl
+pacman -S --noconfirm nouveau-dri xf86-video-nouveau #lib32-nouveau-dri
+
+mkinitcpio -p linux
