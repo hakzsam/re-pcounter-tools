72f06d78ab1c3b7f80837628cca74e65b503ddae
Samuel Pitoiset <samuel.pitoiset@gmail.com>
2014-11-02 19:32:13 +0100

    Update build script

--- build.sh
+++ build.sh
@@ -1,35 +1,60 @@
 #!/bin/bash
-#
-# Build a modified version of valgrind-mmt which displays
-# NVidia ioctl calls.
-#
-VALGRIND="valgrind-mmt"
-PREFIX=$(echo $(pwd)/local)
 
-git clone git://people.freedesktop.org/~mslusarz/valgrind $VALGRIND
-git clone git://people.freedesktop.org/~mslusarz/vex $VALGRIND/VEX
+#
+# Build valgrind-mmt.
+#
+valgrind_dir=valgrind-mmt
+valgrind_git=https://github.com/envytools/valgrind.git
+vex_dir=VEX
+vex_git=https://github.com/envytools/VEX.git
+prefix=$(echo $(pwd)/local)
 
-cd $VALGRIND
-./autogen.sh
-./configure --prefix=$PREFIX
-git apply ../valgrind/trace_nv_ioctls.diff
-make
-make install
-cd ..
+if [ ! -d ${valgrind_dir} ]; then
+    git clone ${valgrind_git} ${valgrind_dir}
+    git clone ${vex_git} ${valgrind_dir}/${vex_dir}
+    cd ${valgrind_dir}
+    cd ${vex_dir} && git checkout -f VEX_3_10_BRANCH && cd ..
+    git checkout -f mmt-3.10
+    cd ..
+else
+    cd ${valgrind_dir} && git fetch origin && git rebase &&
+    cd ${vex_dir} && git fetch origin && git rebase && cd ../..
+fi
+
+cd ${valgrind_dir} && ./autogen.sh && ./configure --prefix=${prefix} \
+                   && make && make install && cd ..
+if [ $? != 0 ]; then
+    echo "Failed to build 'valgrind-mmt'"!
+    exit 1
+fi
 
 #
 # Build envytools.
 #
-git clone git://github.com/envytools/envytools.git
-cd envytools
-mkdir build
-cd build
-cmake ..
-make
-cd ../..
+envytools_dir=envytools
+envytools_git=git://github.com/envytools/envytools.git
+
+if [ ! -d ${envytools_dir} ]; then
+    git clone ${envytools_git} ${envytools_dir}
+else
+    cd ${envytools_dir} && git fetch origin && git rebase && cd ..
+fi
+if [ $? != 0 ]; then
+    echo "Failed to update 'envytools' repo!"
+    exit 1
+fi
+
+cd ${envytools_dir} && mkdir -p build && cd build && cmake .. && make && cd ../..
+if [ $? != 0 ]; then
+    echo "Failed to build 'envytools'"!
+    exit 1
+fi
 
 #
 # Build cupti_trace.
 #
-cd src
-make
+cd src && make && cd ..
+if [ $? != 0 ]; then
+    echo "Failed to build 'cupti_trace'"!
+    exit 1
+fi
