9a06b88bf05b596393b1d015b1ab20d3f9bff7fd
Martin Peres <martin.peres@free.fr>
2015-12-01 18:33:33 +0100

    cupti/trace: add new report for GK208

--- util/enable-mmiotrace.sh
+++ util/enable-mmiotrace.sh
@@ -2,8 +2,6 @@
 
 export LD_LIBRARY_PATH=/opt/cuda/extras/CUPTI/lib64/
 
-echo "Stopping SLIM and X..."
-systemctl stop slim.service
 
 echo "Removing NVidia module..."
 rmmod nvidia
@@ -17,21 +15,23 @@ echo mmiotrace > /sys/kernel/debug/tracing/current_tracer
 echo "Start storing the trace..."
 cat /sys/kernel/debug/tracing/trace_pipe > /home/hakzsam/mmiotrace-dump.txt &
 
+modprobe nvidia
+
 echo "Launching X..."
-X&
+X -config /home/hakzsam/xorg-nvidia-pci1.conf  &
 
 echo "Marking X has been launched..."
 echo "X is up" > /sys/kernel/debug/tracing/trace_marker
 
 echo "cupti start" > /sys/kernel/debug/tracing/trace_marker
-/home/hakzsam/programming/re-pcounter-tools/local/bin/valgrind --tool=mmt --mmt-trace-file=/dev/nvidia0 --mmt-trace-nvidia-ioctls /home/hakzsam/programming/re-pcounter-tools/src/cuda_sample gld_coherent &> /home/hakzsam/valgrind_mmt.trace
+/opt/cuda/extras/CUPTI/sample/callback_event/callback_event 1 sm_cta_launched
 echo "cupti end" > /sys/kernel/debug/tracing/trace_marker
 
 echo nop > /sys/kernel/debug/tracing/current_tracer
 
-cd /home/hakzsam/programming/re-pcounter-tools/src
-./cupti_trace --trace NV86 --event gld_coherent &> /home/hakzsam/cupti_trace_gld_coherent.trace
+#cd /home/hakzsam/programming/re-pcounter-tools/src
+#./cupti_trace --trace NV86 --event gld_coherent &> /home/hakzsam/cupti_trace_gld_coherent.trace
 
-cd /home/hakzsam
-xz -9 mmiotrace-dump.txt
-tar -czvf nv86-full-report-pcounter.tgz mmiotrace-dump.txt.xz valgrind_mmt.trace cupti_trace_gld_coherent.trace
+#cd /home/hakzsam
+#xz -9 mmiotrace-dump.txt
+#tar -czvf nv86-full-report-pcounter.tgz mmiotrace-dump.txt.xz valgrind_mmt.trace cupti_trace_gld_coherent.trace
